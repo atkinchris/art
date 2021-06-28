ea32f3da1e9deb4fcc9c798c5cacc6c76d96f69f
Chris Atkin <atkinchris@gmail.com>
2021-06-28 18:58:33 +0100

    Horizontal bars

--- src/sketch.js
+++ src/sketch.js
@@ -1,27 +1,38 @@
-const sketch = hash => (context, width, height) => {
-  const leftX = Math.floor(width * -0.5)
-  const rightX = Math.floor(width * 1.5)
-  const topY = Math.floor(height * -0.5)
-  const bottomY = Math.floor(height * 1.5)
+const rngBetween = (min, max) => Math.random() * (max - min) + min
 
-  const resolution = Math.floor(width * 0.025)
+const sketch =
+  (/** @type {string} */ hash) =>
+  (/** @type {CanvasRenderingContext2D} */ context, /** @type {number} */ width, /** @type {number} */ height) => {
+    const leftX = Math.floor(width * -0.5)
+    const rightX = Math.floor(width * 1.5)
+    const topY = Math.floor(height * -0.5)
+    const bottomY = Math.floor(height * 1.5)
 
-  const numColumns = Math.floor((rightX - leftX) / resolution)
-  const numRows = Math.floor((bottomY - topY) / resolution)
+    const resolution = Math.floor(width * 0.025)
 
-  const grid = [...Array(numColumns)].map(() => Array(numRows).fill(Math.PI * 0.25))
+    context.globalCompositeOperation = 'screen'
+    context.globalAlpha = 0.5
 
-  grid.forEach((column, columnIndex) => {
-    column.forEach((row, rowIndex) => {
-      const x = columnIndex * resolution
-      const y = rowIndex * resolution
-      const angle = ((rowIndex * 2) / numRows) * Math.PI
+    const drawShape = (x, y, w, h, colour) => {
+      context.fillStyle = colour
 
-      context.moveTo(x, y)
-      context.lineTo(x + 10 * Math.cos(angle), y + 10 * Math.sin(angle))
-      context.stroke()
-    })
-  })
-}
+      const shapeWidth = w * rngBetween(0.9, 1.1)
+      const shapeHeight = h * rngBetween(0.9, 1.2)
+
+      const path = [`M${x} ${y}`]
+
+      path.push(`h ${shapeWidth}`)
+      path.push(`v ${shapeHeight}`)
+      path.push(`h ${-shapeWidth}`)
+
+      path.push('Z')
+
+      context.fill(new Path2D(path.join(' ')))
+    }
+
+    drawShape(100, 10, width * 0.8, height / 25, 'red')
+    drawShape(100, 60, width * 0.8, height / 25, 'blue')
+    drawShape(100, 110, width * 0.8, height / 25, 'yellow')
+  }
 
 export default sketch
