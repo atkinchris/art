f1dea91068e72c8a7f5e4f28072446b2389ed25a
Chris Atkin <atkinchris@gmail.com>
2021-07-22 21:37:15 +0100

    Render markers to build noise function

--- src/sketch.js
+++ src/sketch.js
@@ -1,61 +1,29 @@
-import { rngArrayItem, rngRange } from './utils/random.js'
-
-const colours = ['red', 'blue', 'yellow']
-
-const createShape = () => {
-  const colour = rngArrayItem(colours)
-
-  const shapeWidth = rngRange(500, 750)
-  const shapeHeight = rngRange(40, 70)
-
-  const path = new Path2D([`M0 0`])
-
-  let dX = 0
-  let dY = 0
-  const chaos = rngRange(1, 7)
-
-  for (dX = rngRange(0, 15); dX < shapeWidth; dX += rngRange(0, shapeWidth / 4)) {
-    dY += rngRange(-1 * chaos, 1 * chaos)
-    path.bezierCurveTo(dX, dY, dX, dY, dX, dY)
-  }
-
-  for (dY; dY < shapeHeight; dY += shapeHeight / 6) {
-    dX += rngRange(-1 * chaos, 1 * chaos)
-    path.bezierCurveTo(dX, dY, dX, dY, dX, dY)
-  }
-
-  for (dX = -rngRange(0, 15); dX > 0; dX -= rngRange(0, shapeWidth / 4)) {
-    dY += rngRange(-1 * chaos, 1 * chaos)
-    path.bezierCurveTo(dX, dY, dX, dY, dX, dY)
-  }
-
-  for (dY; dY > 0; dY -= shapeHeight / 6) {
-    dX += rngRange(-1 * chaos, 1 * chaos)
-    path.bezierCurveTo(dX, dY, dX, dY, dX, dY)
-  }
-
-  path.closePath()
-
-  const x = rngRange(-50, 250)
-  const y = rngRange(100, 900) - shapeHeight
-
-  return { path, colour, x, y }
-}
+import { getNoise2D } from './utils/noise.js'
 
 const sketch = () => {
-  const numberOfShapes = rngRange(10, 15)
-  const shapes = Array.from({ length: numberOfShapes }, createShape)
+  const markers = []
+
+  for (let y = 0; y < 1000; y += 25) {
+    for (let x = 0; x < 1000; x += 25) {
+      markers.push({ x, y, value: getNoise2D(x, y) })
+    }
+  }
 
   return (/** @type {CanvasRenderingContext2D} */ context) => {
     context.globalCompositeOperation = 'screen'
     context.globalAlpha = 0.5
 
-    shapes.forEach(shape => {
+    markers.forEach(marker => {
       context.save()
 
-      context.translate(shape.x, shape.y)
-      context.fillStyle = shape.colour
-      context.fill(shape.path)
+      context.translate(marker.x, marker.y)
+      context.fillStyle = 'black'
+
+      const size = marker.value * 10
+
+      context.beginPath()
+      context.arc(size, size, size, 0, Math.PI * 2, true)
+      context.fill()
 
       context.restore()
     })
---
+++ src/utils/noise.js
@@ -0,0 +1,6 @@
+const getNoise2D = (x, y) => {
+  return Math.random()
+}
+
+// eslint-disable-next-line import/prefer-default-export
+export { getNoise2D }
