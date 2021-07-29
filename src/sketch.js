2bc8876f19e7977090e61e4dd6b7847f9994bc5e
Chris Atkin <atkinchris@gmail.com>
2021-07-29 14:50:29 +0100

    Generate a number of blobs

--- src/sketch.js
+++ src/sketch.js
@@ -2,12 +2,16 @@ import { rngRangeFloor } from './utils/random.js'
 import calculateHull from './utils/hull.js'
 import calculateBezierSmoothPath from './utils/interpolation.js'
 
-const sketch = () => {
+const createShape = () => {
+  const width = rngRangeFloor(500, 700)
+  const height = rngRangeFloor(45, 55)
+  const numberOfPoints = rngRangeFloor(30, 60)
+
   const path = new Path2D()
   const hull = calculateHull(
-    Array.from({ length: 50 }, () => ({
-      x: rngRangeFloor(0, 400),
-      y: rngRangeFloor(0, 400),
+    Array.from({ length: numberOfPoints }, () => ({
+      x: rngRangeFloor(-width / 2, width / 2),
+      y: rngRangeFloor(-height / 2, height / 2),
     }))
   )
 
@@ -19,17 +23,39 @@ const sketch = () => {
 
   path.closePath()
 
+  return {
+    x: rngRangeFloor(300, 600),
+    y: 0,
+    path,
+  }
+}
+
+const sketch = () => {
+  const spacingFactor = 1.3
+  const numberOfShapes = rngRangeFloor(20, 30)
+  const shapes = Array.from({ length: numberOfShapes }, createShape)
+
+  const spacingMax = (1000 * spacingFactor) / numberOfShapes
+
+  shapes.forEach((shape, index) => {
+    const { y: previousY = spacingMax } = shapes[index - 1] || {}
+    // eslint-disable-next-line no-param-reassign
+    shape.y += previousY + rngRangeFloor(10, spacingMax)
+  })
+
   return (/** @type {CanvasRenderingContext2D} */ context) => {
     context.globalCompositeOperation = 'screen'
     context.globalAlpha = 0.5
-    context.fillStyle = 'black'
 
-    context.save()
+    shapes.forEach(shape => {
+      context.save()
 
-    context.translate(300, 300)
-    context.stroke(path)
+      context.fillStyle = 'black'
+      context.translate(shape.x, shape.y)
+      context.fill(shape.path)
 
-    context.restore()
+      context.restore()
+    })
   }
 }
 
